0c65ca7252e2632698b51d8f312590d56ebb3935
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-09 22:52:45 +0100

    Add option tu customize Section style

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -1,6 +1,6 @@
-export default function Section ({ children }) {
+export default function Section ({ children, flexRow }) {
   return (
-    <section className="w-full px-6 flex justify-evenly md:px-0 sm:max-w-6xl pt-32">
+    <section className={`w-full px-6 flex ${flexRow ? 'flex-row' : 'flex-col'} justify-evenly md:px-0 sm:max-w-6xl pt-32`}>
       {children}
     </section>
   )
