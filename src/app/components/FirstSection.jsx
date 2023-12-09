187cf0b1c5eebbc6109cdc6cae67851058b78a6b
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-09 15:23:18 +0100

    changes on tags and fix flex display

--- src/app/components/FirstSection.jsx
+++ src/app/components/FirstSection.jsx
@@ -10,13 +10,13 @@ export default function FirstSection () {
         <div className="flex flex-col gap-y-7 md:hidden">
           <div className='flex flex-col gap-x-10'>
             <div className='flex'>
-              <p className='text-[14px] font-medium text-black opacity-50'>
+              <strong className='text-[14px] font-light text-black opacity-50'>
                 Powered by:
                 <span className='text-[14px] font-medium text-black'>
                   &nbsp;
                   Zeptolab
                 </span>
-              </p>
+              </strong>
             </div>
             <Image
               className='h-20 w-20'
@@ -27,28 +27,28 @@ export default function FirstSection () {
             />
           </div>
           <div className='border border-[#DADADA] rounded px-10 py-5 flex flex-col gap-y-2'>
-            <h3 className='text-[#685DC5] font-medium text-[16px]'>Application closes in</h3>
-            <h3 className='text-[#535353] font-light text-[20px]'>6 Day  :  22 Hrs  :  56 Min  :  13 Seg</h3>
+            <strong className='text-[#685DC5] font-medium text-[16px]'>Application closes in</strong>
+            <strong className='text-[#535353] font-light text-[20px]'>6 Day  :  22 Hrs  :  56 Min  :  13 Seg</strong>
           </div>
           <div className='border border-[#DADADA] rounded px-5 py-5 flex flex-col gap-y-4'>
             <div className='flex justify-between'>
-              <div className='w-48'>
-                <h3 className='text-[#685DC5] font-medium text-[16px]'>Location</h3>
-                <h3 className='text-[#535353] font-light text-[16px]'>Bangkok</h3>
+              <div className='w-48 flex flex-col'>
+                <strong className='text-[#685DC5] font-medium text-[16px]'>Location</strong>
+                <strong className='text-[#535353] font-light text-[16px]'>Bangkok</strong>
               </div>
-              <div className='w-48'>
-                <h3 className='text-[#685DC5] font-medium text-[16px]'>Duration</h3>
-                <h3 className='text-[#535353] font-light text-[16px]'>1 year Full-Time</h3>
+              <div className='w-48 flex flex-col'>
+                <strong className='text-[#685DC5] font-medium text-[16px]'>Duration</strong>
+                <strong className='text-[#535353] font-light text-[16px]'>1 year Full-Time</strong>
               </div>
             </div>
             <div className='flex justify-between'>
-              <div className='w-48'>
-                <h3 className='text-[#685DC5] font-medium text-[16px]'>Application deadline</h3>
-                <h3 className='text-[#535353] font-light text-[16px]'>30 June 2020</h3>
+              <div className='w-48 flex flex-col'>
+                <strong className='text-[#685DC5] font-medium text-[16px]'>Application deadline</strong>
+                <strong className='text-[#535353] font-light text-[16px]'>30 June 2020</strong>
               </div>
-              <div className='w-48'>
-                <h3 className='text-[#685DC5] font-medium text-[16px]'>Start date</h3>
-                <h3 className='text-[#535353] font-light text-[16px]'>3 Aug 2020</h3>
+              <div className='w-48 flex flex-col'>
+                <strong className='text-[#685DC5] font-medium text-[16px]'>Start date</strong>
+                <strong className='text-[#535353] font-light text-[16px]'>3 Aug 2020</strong>
               </div>
             </div>
           </div>
@@ -58,13 +58,13 @@ export default function FirstSection () {
           <p className="text-[#535353] text-[22px] font-medium">A fully funded work-study program to launch your tech career</p>
           <p className="text-[#535353] text-[22px] font-light">Harbour.Space has partnered with SCG to empower driven talent and eliminate the barriers to accessing exceptional education and career opportunities through a Masters Fellowship.</p>
           <p className="block text-[#535353] text-[22px] font-light md:hidden">Scholarship candidates will receive full financial support to complete their Masters program at Harbour.Space while gaining invaluable work experience through an internship with SCG, a leading company in the industry.</p>
-          <p className='text-[22px] font-medium text-[#535353]'>
+          <strong className='text-[22px] font-medium text-[#535353]'>
             Position:
             <span className='font-light text-[#535353]'>
               &nbsp;
               Marketing Performance
             </span>
-          </p>
+          </strong>
         </div>
         <div>
           <button className='bg-[#685dc5] text-white rounded-full py-3 px-8 md:capitalize'>
@@ -91,38 +91,38 @@ export default function FirstSection () {
             height={24}
           />
           <div className='flex flex-col justify-center'>
-            <p className='text-lg font-light text-black opacity-50'>
+            <strong className='text-lg font-light text-black opacity-50'>
               Powered by:
-            </p>
-            <span className='text-[27px] font-light text-[#535353]'>
+            </strong>
+            <strong className='text-[27px] font-light text-[#535353]'>
               Zeptolab
-            </span>
+            </strong>
           </div>
         </div>
         <div className='border border-[#DADADA] bg-white rounded px-10 py-5 flex flex-col gap-y-4'>
-          <h3 className='text-[#685DC5] font-medium text-lg'>Application closes in</h3>
-          <h3 className='text-[#535353] font-light text-[27px]'>6 Day  :  22 Hrs  :  56 Min  :  13 Seg</h3>
+          <strong className='text-[#685DC5] font-medium text-lg'>Application closes in</strong>
+          <strong className='text-[#535353] font-light text-[27px]'>6 Day  :  22 Hrs  :  56 Min  :  13 Seg</strong>
         </div>
         <div className='border border-[#DADADA] bg-white rounded px-10 py-10 flex flex-col z-10'>
           <div className='flex justify-between'>
-            <div className='w-48'>
-              <h3 className='text-[#685DC5] font-medium text-[16px]'>Location</h3>
-              <h3 className='text-[#535353] font-light text-[16px]'>Bangkok</h3>
+            <div className='w-48 flex flex-col'>
+              <strong className='text-[#685DC5] font-medium text-[16px]'>Location</strong>
+              <strong className='text-[#535353] font-light text-[16px]'>Bangkok</strong>
             </div>
-            <div className='w-48'>
-              <h3 className='text-[#685DC5] font-medium text-[16px]'>Duration</h3>
-              <h3 className='text-[#535353] font-light text-[16px]'>1 year</h3>
-              <h3 className='text-[#535353] font-light text-[16px]'>Full-Time</h3>
+            <div className='w-48 flex flex-col'>
+              <strong className='text-[#685DC5] font-medium text-[16px]'>Duration</strong>
+              <strong className='text-[#535353] font-light text-[16px]'>1 year</strong>
+              <strong className='text-[#535353] font-light text-[16px]'>Full-Time</strong>
             </div>
           </div>
           <div className='flex justify-between'>
-            <div className='w-48'>
-              <h3 className='text-[#685DC5] font-medium text-[16px]'>Start date</h3>
-              <h3 className='text-[#535353] font-light text-[16px]'>20 June 2020</h3>
+            <div className='w-48 flex flex-col'>
+              <strong className='text-[#685DC5] font-medium text-[16px]'>Start date</strong>
+              <strong className='text-[#535353] font-light text-[16px]'>20 June 2020</strong>
             </div>
-            <div className='w-48'>
-              <h3 className='text-[#685DC5] font-medium text-[16px]'>End date</h3>
-              <h3 className='text-[#535353] font-light text-[16px]'>3 Aug 2020</h3>
+            <div className='w-48 flex flex-col'>
+              <strong className='text-[#685DC5] font-medium text-[16px]'>End date</strong>
+              <strong className='text-[#535353] font-light text-[16px]'>3 Aug 2020</strong>
             </div>
           </div>
         </div>
