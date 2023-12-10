f77819ccbfc25bec603cb4b6ee966b016721606f
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-10 18:51:05 +0100

    add prop 'paddingX' to Section Component

--- src/app/components/Section.jsx
+++ src/app/components/Section.jsx
@@ -1,6 +1,6 @@
-export default function Section ({ children, flexDirection = 'flex-row' }) {
+export default function Section ({ children, flexDirection = 'flex-row', paddingX = '' }) {
   return (
-    <section className={`w-full px-6 flex ${flexDirection} justify-evenly sm:max-w-6xl pt-32`}>
+    <section className={`w-full flex ${flexDirection} justify-evenly ${paddingX} pt-32 sm:max-w-6xl`}>
       {children}
     </section>
   )
