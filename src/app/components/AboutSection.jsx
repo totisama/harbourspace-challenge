f7e46645ef5f0bcb5721f208d00f3946d9ac53e7
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-10 18:53:52 +0100

    Add responsive design to first part of About Component

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -5,18 +5,18 @@ import PairedInformation from './PairedInformation'
 
 export default function AboutSection () {
   return (
-    <Section flexDirection='flex-col'>
-      <div className="w-11/12 flex flex-col items-center gap-10 lg:gap-32 lg:flex-row">
+    <Section flexDirection='flex-col' paddingX='px-0'>
+      <div className="w-11/12 px-6 hidden sm:flex flex-col items-center gap-10 lg:gap-32 lg:flex-row">
         <div className='flex'>
           <div className="flex justify-center items-center h-[438px] w-[438px] bg-[url('/svgs/circularPattern.svg')] bg-no-repeat">
             <Image
               className='h-96 w-96'
-              src="/images/aboutImage.png"
-              alt="Zeptolab logo"
+              src="/images/student.png"
+              alt="Student image"
               width={1000}
               height={1000}
               priority
-              />
+            />
           </div>
         </div>
         <div className='flex flex-col gap-y-10'>
@@ -24,14 +24,36 @@ export default function AboutSection () {
           <p className='font-light text-[22px] text-[#535353]'>Our scholarships are designed to give talented and driven young people from any background access to top-class education, experience and network. We offer a fully-funded master’s degree alongside an apprenticeship and a guaranteed job upon graduation.</p>
         </div>
       </div>
-      <div className='w-full flex flex-col md:flex-row mt-10 md:mt-20 gap-10'>
+
+      <div className='w-full mt-36 flex sm:hidden'>
+        <div className='w-[4%] h-[450px] bg-[#685DC5] mt-2'></div>
+        <div className='relative w-[92%] border border-[#E6E6E6] bg-white px-5 pt-72 pb-14'>
+          <div className='flex flex-col gap-y-8'>
+            <h2 className='font-medium text-4xl tracking-wide text-[#685DC5]'>About the apprenticeship</h2>
+            <p className='font-light text-[18px] leading-6 text-[#535353]'>Our scholarships are designed to give talented and driven young people from any background access to top-class education, experience and network. We offer a fully-funded master’s degree alongside an apprenticeship and a guaranteed job upon graduation.</p>
+          </div>
+          <div className="absolute flex justify-center items-center h-[450px] w-[450px] top-10 -translate-x-1/2 -translate-y-1/2 left-1/2 bg-[url('/svgs/circularPattern.svg')] bg-no-repeat -z-10" />
+          <Image
+            className='absolute top-10 -translate-x-1/2 -translate-y-1/2 left-1/2 h-96 w-96 z-10'
+            src="/images/building.png"
+            alt="Building image"
+            width={1000}
+            height={1000}
+            priority
+          />
+        </div>
+        <div className='w-[4%] h-[450px] bg-[#685DC5] mt-2'></div>
+      </div>
+
+      <div className='w-full px-6 flex flex-col md:flex-row mt-10 md:mt-20 gap-10'>
 
         {/* left */}
         <div className='w-1/3 border hidden flex-col border-[#DADADA] justify-end rounded pt-5 pb-10 px-10 gap-y-7 md:flex'>
-          <div className='flex flex-col mb-40 gap-y-5'>
+          <div className='flex flex-col gap-y-5'>
             <strong className='text-base font-medium text-[#685DC5]'>Scholarship value</strong>
             <strong className='text-5xl font-light text-[#535353]'>€31,300</strong>
           </div>
+          <div className='flex-1'/>
           <hr className="w-full h-[1px] mx-auto border-0 bg-[#DADADA]" />
           <div className='flex flex-col gap-10'>
             <div className='flex gap-10'>
