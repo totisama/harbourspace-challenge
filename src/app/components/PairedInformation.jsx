090d6d87b5e7db3126f14eb91813250273b86d29
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-10 13:01:54 +0100

    Add new textSize prop to PairedInformation Component

--- src/app/components/PairedInformation.jsx
+++ src/app/components/PairedInformation.jsx
@@ -1,8 +1,8 @@
-export default function PairedInformation ({ title, text, width = '', extra = false, extraText }) {
+export default function PairedInformation ({ title, text, textSize, width = '', extra = false, extraText }) {
   return (
     <div className={`${width} flex flex-col`}>
       <strong className='text-base font-medium text-[#685DC5]'>{title}</strong>
-      <strong className='text-base font-light text-[#535353]'>{text}</strong>
+      <strong className={`${textSize || 'text-base'} font-light text-[#535353]`}>{text}</strong>
       {extra
         ? <strong className='text-base font-light text-[#535353]'>{extraText}</strong>
         : null
