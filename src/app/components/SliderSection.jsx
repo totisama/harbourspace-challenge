1f1b0a15da1335162165cddce19ee23f140901b5
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-12-11 11:56:01 +0100

    Add SliderSection Component

---
+++ src/app/components/SliderSection.jsx
@@ -0,0 +1,9 @@
+import Section from './Section'
+
+export default function SliderSection () {
+  return (
+    <Section>
+      <h1>Slider</h1>
+    </Section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import AboutSection from './components/AboutSection'
 import FAQSection from './components/FAQSection'
 import FirstSection from './components/FirstSection'
+import SliderSection from './components/SliderSection'
 
 export default function Home () {
   return (
     <main className="flex flex-col justify-center items-center pb-28">
       <FirstSection />
       <AboutSection />
+      <SliderSection />
       <FAQSection />
     </main>
   )
